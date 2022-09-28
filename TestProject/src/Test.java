accbebc92709b0b7b3ec24d30b6bb172e6cc6cc9
2521112884 <2521112884@NB1990081.oa.bbl>
2022-09-28 14:42:27 +0700

    commit class Test.java

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -2,6 +2,12 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+
+		System.out.println("one");
+		System.out.println("two");
+		System.out.println("three");
+
+		
 	}
+
 }
